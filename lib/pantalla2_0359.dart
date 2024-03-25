e601e685cc4b9d02455e57522c98452f81f6a7d6
GarciaJ128 <143743623+GarciaJ128@users.noreply.github.com>
2024-03-25 10:06:39 -0700

    Update pantalla2_0359.dart

--- lib/pantalla2_0359.dart
+++ lib/pantalla2_0359.dart
@@ -1,5 +1,4 @@
 //Pantalla2_0359
-
 import 'package:flutter/material.dart';
 
 class Pantalla2_0359 extends StatelessWidget {
@@ -9,16 +8,45 @@ class Pantalla2_0359 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Pantalla 2 Garcia0359"),
-        backgroundColor: Colors.black,
+        title: const Text("Pantalla 2 Garcia0359",
+            style: TextStyle(color: Colors.white)),
+        backgroundColor: const Color(0xFFB71C1C),
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ElevatedButton(
-              onPressed: () {}, //si presiona boton
-              child: Text("Pantalla"),
+            Container(
+              height: 100,
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                color: Color(0xFFC62828),
+                borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(50),
+                  bottomLeft: Radius.circular(50),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Color(0Xffe57373),
+                    offset: Offset(9, 9),
+                    blurRadius: 4,
+                  ),
+                ],
+              ),
+              alignment: Alignment.center,
+              child: const Text(
+                'Mi encabezado',
+                style: TextStyle(
+                  fontSize: 35,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            const Text("Jennifer Garcia",
+                style: TextStyle(fontSize: 20, color: Color(0xff610a0a))),
+            Container(
+              margin: const EdgeInsets.all(20.0),
+              child: const Text("Encabezado Mat. 21308051280359",
+                  style: TextStyle(fontSize: 20, color: Color(0xff610a0a))),
             )
           ],
         ),
